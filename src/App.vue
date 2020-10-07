ac0d68eec9a1e0dca29aebff1e6cc6b2ac409206
andiedioso <andie.dioso1@gmail.com>
2020-10-07 15:07:21 +0800

    made the task-app in one component first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Tasks </h1>
+    <section>
+      <table>
+        <thead>
+        <th>Task Name</th>
+        <th>Status</th>
+        </thead>
+
+        <tbody v-for="(task, index) in taskList" :key="index">
+        <tr>
+          <td> {{task.name}} </td>
+          <td> {{task.status}} </td>
+          <button v-if="task.status=='Pending'" @click="markAsComplete(index)">Mark as Complete</button>
+          <button @click="removeTask(index)"> Remove Task </button>
+        </tr>
+        </tbody>
+
+      </table>
+    </section>
+
+    <section>
+      <input type="text" v-model="nameInput">
+
+      <button v-if="isEmpty()==true" disabled>Add Task</button>
+      <button v-if="isEmpty()==false" @click="addTask">Add Task</button>
+      <button @click="clearInput()">Clear</button>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      taskList: [
+      ],
+      nameInput: '',
+      statusInput: 'Pending',
+    }
+  },
+  methods:{
+    markAsComplete(idx){
+      this.taskList[idx].status = 'Completed';
+    },
+    addTask(){
+      this.taskList.push({name:this.nameInput, status:'Pending'});
+      this.nameInput = '';
+    },
+    removeTask(idx) {
+      this.taskList.splice(idx, 1);
+    },
+    isEmpty(){
+      if(this.nameInput==""){
+        return true;
+      } else{
+        return false;
+      }
+    },
+    clearInput(){
+      this.nameInput = '';
+    }
   }
 }
 </script>
