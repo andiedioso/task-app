e0919854c65789ea899314a5b7846ba4a35434d6
andiedioso <andie.dioso1@gmail.com>
2020-10-08 13:52:02 +0800

    exported component lmao

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,59 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1> Tasks </h1>
-    <section>
-      <table>
-        <thead>
-        <th>Task Name</th>
-        <th>Status</th>
-        </thead>
 
-        <tbody v-for="(task, index) in taskList" :key="index">
-        <tr>
-          <td> {{task.name}} </td>
-          <td> {{task.status}} </td>
-          <button v-if="task.status=='Pending'" @click="markAsComplete(index)">Mark as Complete</button>
-          <button @click="removeTask(index)"> Remove Task </button>
-        </tr>
-        </tbody>
-      </table>
-      <p> {{ message }}</p>
-    </section>
+<!--    <section>-->
+<!--      <table>-->
+<!--        <thead>-->
+<!--        <th>Task Name</th>-->
+<!--        <th>Status</th>-->
+<!--        </thead>-->
 
-    <section>
-      <input type="text" v-model="nameInput">
+<!--        <tbody v-for="(task, index) in taskList" :key="index">-->
+<!--        <tr>-->
+<!--          <td> {{task.name}} </td>-->
+<!--          <td> {{task.status}} </td>-->
+<!--          <button v-if="task.status=='Pending'" @click="markAsComplete(index)">Mark as Complete</button>-->
+<!--          <button @click="removeTask(index)"> Remove Task </button>-->
+<!--        </tr>-->
+<!--        </tbody>-->
+<!--      </table>-->
+<!--      <p> {{ message }}</p>-->
+<!--    </section>-->
 
-      <button v-if="isEmpty()==true" disabled>Add Task</button>
-      <button v-if="isEmpty()==false" @click="addTask">Add Task</button>
-      <button @click="clearInput()">Clear</button>
-    </section>
+<!--    <section>-->
+<!--      <input type="text" v-model="nameInput">-->
+
+<!--      <button v-if="isEmpty()==true" disabled>Add Task</button>-->
+<!--      <button v-if="isEmpty()==false" @click="addTask">Add Task</button>-->
+<!--      <button @click="clearInput()">Clear</button>-->
+<!--    </section>-->
+      <TaskItem></TaskItem>
+
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import TaskItem from './components/TaskItem.vue'
 
 export default {
   name: 'App',
+  components: {
+        TaskItem
+      },
   data () {
     return {
-      taskList: [
+      taskList: [{
+        name: 'Task A',
+        status: 'Pending',
+      }, {
+        name: 'Task B',
+        status: 'Complete',
+      }, {
+        name: 'Task C',
+        status: 'Pending',
+      }
+
       ],
       nameInput: '',
       statusInput: 'Pending',
